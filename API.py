22c3aff0ef6d704a2750fb56c19403f8fe3ea920
ayaabe95 <ayabarjawi@gmail.com>
2022-06-28 01:42:11 +0300

    insert data into tables

--- API.py
+++ API.py
@@ -11,7 +11,8 @@ cursor = conn.cursor()
 
 app = Flask(__name__)
 class API:
-   
+    ### method for creating tables ###
+
     '''http://127.0.0.1:5000/create?tableName=products'''
     @app.route('/create')
     def create_table():
@@ -61,8 +62,96 @@ class API:
                 cursor.close()
                 conn.close()
 
+    ### methods for inserting data into tables ###
+
+    '''http://127.0.0.1:5000/addProduct?ProductName=ddd&ProductPrice=60'''
+    @app.route('/addProduct',methods=['POST'])
+    def insert_product():
+        global conn
+        global cursor
+        ProductName=str(request.args.get('ProductName'))
+        ProductPrice=int(request.args.get('ProductPrice'))
+
+        sql = """INSERT INTO Products(ProductName,ProductPrice)
+                 VALUES(%s,%s) RETURNING ProductID;"""
+        ProductID = None
+        try:
+            cursor.execute(sql, (ProductName,ProductPrice,))
+            # get the generated id back
+            ProductID = cursor.fetchone()[0]
+            conn.commit()
+            cursor.close()
+
+        except (Exception, psycopg2.Error) as error:
+            return "Failed to insert product ", error
+
+        finally:
+            if conn:
+                cursor.close()
+                conn.close()
+        return ProductName + ' inserted successfully into products\' table'
+    
+    '''http://127.0.0.1:5000/addCustomer?CustomerName=ahmad&ContactNumber=666&Country=Jordan&City=Irbid'''
+    @app.route('/addCustomer',methods=['POST'])
+    def insert_customer():
+        global conn
+        global cursor
+        CustomerName=str(request.args.get('CustomerName'))
+        ContactNumber=int(request.args.get('ContactNumber'))
+        Country=str(request.args.get('Country'))
+        City=str(request.args.get('City'))
+
+        sql = """INSERT INTO Customers(CustomerName,ContactNumber,Country,City)
+                 VALUES(%s,%s,%s,%s) RETURNING CustomerID;"""
+        CustomerID = None
+        try:
+            cursor.execute(sql, (CustomerName,ContactNumber,Country,City))
+            # get the generated id back
+            CustomerID = cursor.fetchone()[0]
+            conn.commit()
+            cursor.close()
+
+        except (Exception, psycopg2.Error) as error:
+            return "Failed to insert customer ", error
+
+        finally:
+            if conn:
+                cursor.close()
+                conn.close()
+        return CustomerName + ' inserted successfully into customers\' table'
+    
+    '''http://127.0.0.1:5000/createOrder?OrderDate=2022-06-04&CustomerID=1&ProductID=1'''
+    @app.route('/createOrder',methods=['POST'])
+    def create_order():
+        global conn
+        global cursor
+        OrderDate=str(request.args.get('OrderDate'))
+        CustomerID=int(request.args.get('CustomerID'))
+        ProductID=int(request.args.get('ProductID'))
+
+        sql = """INSERT INTO Orders(OrderDate,CustomerID,ProductID)
+                 VALUES(%s,%s,%s) RETURNING OrderID;"""
+        OrderID = None
+        try:
+            cursor.execute(sql, (OrderDate,CustomerID,ProductID))
+            # get the generated id back
+            OrderID = cursor.fetchone()[0]
+            conn.commit()
+            cursor.close()
+
+        except (Exception, psycopg2.Error) as error:
+            return "Failed to create order ", error
+
+        finally:
+            if conn:
+                cursor.close()
+                conn.close()
+        return 'Order '+ str(OrderID) + ' created successfully'
+
 if __name__=="__main__":
     app.run(debug=True)
 
 api=API()
 api.create_table
+#api.insert_product
+#api.insert_customer
